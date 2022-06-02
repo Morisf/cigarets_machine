621f3b5a3868057634c89056ecf384f0a2c8eca2
Moris Finkel <moris@moirs.org.ua>
2022-06-02 13:01:45 +0200

    Add test for Command

---
+++ tests/Unit/Command/PurchaseCigarettesCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\Unit\Command;
+
+use App\Exceptions\NotEnoughMoneyException;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class PurchaseCigarettesCommandTest extends KernelTestCase
+{
+    public function testSuccess(): void
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('app:purchase-cigarettes');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'amount' => 2,
+            'credit' => 10.00
+        ]);
+
+        $commandTester->assertCommandIsSuccessful();
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Your change is: 0.02€', $output);
+        $this->assertStringContainsString('| 2     | 1     |', $output);
+    }
+
+    public function testFailed(): void
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+        $this->expectException(NotEnoughMoneyException::class);
+        $command = $application->find('app:purchase-cigarettes');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'amount' => 2,
+            'credit' => 0
+        ]);
+    }
+}
